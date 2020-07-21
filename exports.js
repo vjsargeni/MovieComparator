0491f1b7b327d10785fb29886259eddbc50d52be
vjsargeni <vsargeni@udel.edu>
2020-07-21 19:02:51 -0400

    Added some filtering, exportJS to aid modularity

---
+++ exports.js
@@ -0,0 +1,17 @@
+module.exports = {
+    //containing all the input data for main.js
+    selectors : {
+    //collection of selectors 
+    //imdb
+    titleSelector: '#main > div > span > div > div > div.lister > table > tbody > tr:nth-child(1) > td.titleColumn > a',
+    ratingSelector: '#main > div > span > div > div > div.lister > table > tbody > tr:nth-child(1) > td.ratingColumn.imdbRating > strong',
+    yearSelector: '#main > div > span > div > div > div.lister > table > tbody > tr:nth-child(1) > td.titleColumn > span'
+    },
+
+    urls: {
+        //imdb
+        top: 'https://www.imdb.com/chart/top',
+        bottom: 'https://www.imdb.com/chart/bottom',
+        
+    }
+}
--- main.js
+++ main.js
@@ -1,26 +1,50 @@
 const puppeteer = require('puppeteer');
 const axios = require('axios');
+const ex = require('./exports.js');
 
-var movies =[];
+let movies =[];
 
 (async () => {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
+
+    const url = ex.urls.bottom;
+    let numOfMovies = 100;
   
     // Instructs the blank page to navigate a URL
-    await page.goto('https://www.imdb.com/chart/moviemeter/?ref_=nv_mv_mpm');
+    await page.goto(ex.urls.top);
     
     //wait for first selector to load
-    await page.waitForSelector('#main > div > span > div > div > div.lister > table > tbody > tr:nth-child(1) > td.titleColumn > a');
-    for(var i = 1; i <= 250; i++){
-      let element = await page.$('#main > div > span > div > div > div.lister > table > tbody > tr:nth-child('+ i + ') > td.titleColumn > a');
-      let title = await page.evaluate(el => el.textContent, element)
-      movies.push(title);
+    await page.waitForSelector(ex.selectors.titleSelector);
+
+    if (url === ex.urls.top){
+      numOfMovies = 250
     }
-    
-    for(el in movies){
-      console.log(el);
+
+    for(var i = 1; i <= numOfMovies; i++){
+      
+      let name = await page.$(ex.selectors.titleSelector.replace('tr:nth-child(1)', 'tr:nth-child('+ i +')'));
+      let score = await page.$(ex.selectors.ratingSelector.replace('tr:nth-child(1)', 'tr:nth-child('+ i +')'));
+      let released = await page.$(ex.selectors.yearSelector.replace('tr:nth-child(1)', 'tr:nth-child('+ i +')'));
+
+      let title = await page.evaluate(el => el.textContent, name);
+      let rating = await page.evaluate(el => el.textContent, score);
+      let year = await page.evaluate(el => el.textContent, released);
+
+
+      movies.push({
+        title: title,
+        rating: parseFloat(rating),
+        year: parseInt(year.replace('(','').replace(')',''))
+      });
     }
+    movies.sort((x,y) => x.rating - y.rating);
+
+    movies.forEach(function(el){
+      if (el.year >= 2000){
+        console.log(el);
+      }
+    });
     
 
     //await getOMDB(title);
